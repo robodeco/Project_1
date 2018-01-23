9158365d68da754ae2e7765defdd89e3740b3597
Alex H <08ahayden@gmail.com>
2018-01-22 19:41:06 -0500

    Added checker/unchecker function for cuisine buttons as well as rendered the buttons with JS instead of hard coding them out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,25 +21,25 @@ if (navigator.geolocation) {
 function showPosition(position) {
   lat = position.coords.latitude.toString();
   long = position.coords.longitude.toString();
-  console.log(lat);
-  console.log(long);
+  // console.log(lat);
+  // console.log(long);
 
   $(".cuisineButton").on("click", function() {
     cuisineVal +=$(this).val();
 
     finalQueryURL = queryBaseURL + "search?q=" + cuisineVal + "lat=" + lat + "&lon=" + long
 
-    console.log(cuisineVal);
-    console.log(finalQueryURL);
+    // console.log(cuisineVal);
+    // console.log(finalQueryURL);
   })
 
 }
 
-console.log("https://developers.zomato.com/api/v2.1/search?lat=38.832801&lon=-77.196229&cuisines=american%20steak%20brazilian");
+// console.log("https://developers.zomato.com/api/v2.1/search?lat=38.832801&lon=-77.196229&cuisines=american%20steak%20brazilian");
 
     $("#submit").on("click", function() {
-      console.log("hi world");
-      console.log(finalQueryURL);
+      // console.log("hi world");
+      // console.log(finalQueryURL);
       $.ajax({
         url: finalQueryURL,
         method: "GET",
@@ -50,16 +50,16 @@ console.log("https://developers.zomato.com/api/v2.1/search?lat=38.832801&lon=-77
       })
       .done(function(response) {
 
-      console.log(response.restaurants);
+      // console.log(response.restaurants);
 
        var results = response.restaurants;
 
        for (var i = 0; i < results.length; i++) {
 
        var restName = response.restaurants[i].restaurant.name
-       console.log(restName);
+       // console.log(restName);
        var restWeb = response.restaurants[i].restaurant.url
-       console.log(restWeb);
+       // console.log(restWeb);
        var restLoca = response.restaurants[i].restaurant.location
 
         $("#restaurants").append("<div><p>" + restName + "</p><p>" + restWeb + "</p><p>" + restLoca + "</p><p>");
--- assets/javascript/indexjs.js
+++ assets/javascript/indexjs.js
@@ -12,34 +12,86 @@ var database = firebase.database();
 
 database.ref().child("Users");
 
-
-var userprefs ={
-
- timeofday : "",
- culture : "",
- price : ""
+var culturepick = "";
+var userprefs = {
+  // timeofday : "",
+  culture: "",
+  price: ""
 }
 
+var cuisines = ["American", "Italian", "Mexican", "Chinese", "Thai", "Afghanistan", "African", "Brazilian", "Caribbean", "European", "Ethiopian", "Filipino", "Indonesian", "Japanese", "Lebanese", "Mediterranean", "Moroccan", "Peruvian", "Portuguese", "Russian", "Vietnamese", "Vegetarian"]
+var CuisinesButtonsFinished = [];
+
+
+//create cuisine buttons
+function renderbuttons() {
+  culturepick = "";
+  $("#BtnDisplay").empty();
+  CuisinesButtonsFinished = [];
+  for (var i = 0; i < cuisines.length; i++) {
+    var btns = $("<button>");
+    var values = cuisines[i].toLowerCase();
+    btns.addClass("btn btn-primary cuisineButton");
+    btns.attr("checker", "unchecked");
+    btns.attr("value", values);
+    btns.text(cuisines[i]);
+    $("#BtnDisplay").append(btns);
+    CuisinesButtonsFinished.push(btns);
+
+  };
+};
+
+renderbuttons();
+
+
+//if cuisine button is clicked, give it a attr of "checked". If it is unclicked,change to "unclicked"
+function checkFunction() {
+  var ischecked = $(this).attr("checker");
+  if (ischecked === "unchecked") {
+    $(this).attr("checker", "checked");
+  } else {
+    $(this).attr("checker", "unchecked");
+  }
+}
+
+//click handler for all cuisine buttons to run the check function
+$(document).on("click", ".cuisineButton", checkFunction);
+
+
+//pull previous search terms from firebase for logged in user
 firebase.auth().onAuthStateChanged(function(user) {
-  firebase.database().ref('Users/' + user.displayName).on("value", function (snapshot){
+  firebase.database().ref('Users/' + user.displayName).on("value", function(snapshot) {
     console.log(snapshot.val());
     var prevculture = snapshot.val().culture;
     var prevprice = snapshot.val().price;
-    var prevtimeofday = snapshot.val().timeofday;
-    console.log (prevculture + "," + prevprice + "," + prevtimeofday);
+    // var prevtimeofday = snapshot.val().timeofday;
+    console.log(prevculture + "," + prevprice);
   });
 });
 
 
+function GetCuisinePrefs() {
+  for (var j = 0; j < CuisinesButtonsFinished.length; j++) {
+    var newchecker = CuisinesButtonsFinished[j].attr("checker");
+    if (newchecker === "checked") {
+      culturepick += CuisinesButtonsFinished[j].val().trim() + "&";
+    };
+  };
+};
 
-$("#submit").on("click", function(){
+
+$("#submit").on("click", function() {
   event.preventDefault();
+  GetCuisinePrefs();
+  console.log(culturepick);
+  userprefs.culture = culturepick;
+  renderbuttons();
 
-        userprefs.timeofday = $("#time-input").val();
-        userprefs.culture = $("#culture-input").val();
-        userprefs.price = $("#price-input").val();
+  // userprefs.timeofday = $("#time-input").val();
+  // userprefs.culture = culturepickpicker();
+  userprefs.price = $("#price-input").val();
 
-        firebase.auth().onAuthStateChanged(function(user) {
-          firebase.database().ref('Users/' + user.displayName).set(userprefs);
-        });
+  firebase.auth().onAuthStateChanged(function(user) {
+    firebase.database().ref('Users/' + user.displayName).set(userprefs);
+  });
 });
