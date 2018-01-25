89f07d65518e743345a01e443876697d29d596cd
Alex H <08ahayden@gmail.com>
2018-01-24 20:47:29 -0500

    Current deployed page added to signin JS

--- assets/javascript/signinpage.js
+++ assets/javascript/signinpage.js
@@ -48,7 +48,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: 'file:///C:/Users/08aha/Documents/Coding_Master/Project_1/index.html',
+  signInSuccessUrl: 'https://scruffylookingnerfherder.github.io/Project_1/',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
